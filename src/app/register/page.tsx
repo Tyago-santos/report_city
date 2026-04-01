477a0b39714ae3f3c9523535209e3d57dbe582d7
Tiago Santos <xlzthyago@gmail.com>
2026-04-01 20:49:16 -0300

    criar register

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/src/components/registerComponent";
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+}
---
+++ src/components/registerComponent.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { loginSchema } from "../validation/registerValidation";
+import { handleFormData } from "../utils/helpers/registerHandle";
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(handleFormData)}>
+      <FieldGroup className="w-[300px]">
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+          <Input
+            {...register("name")}
+            id="fieldgroup-name"
+            placeholder="Jordan Lee"
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )}
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+          <Input
+            {...register("email")}
+            id="fieldgroup-email"
+            type="text"
+            placeholder="name@example.com"
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
+          <FieldDescription>
+            <Link href="/forget">Esqueceu sua senha?</Link>
+          </FieldDescription>
+        </Field>
+        <Field orientation="horizontal">
+          <Button
+            className="w-full cursor-pointer hover:bg-white hover:border-black border-2 hover:text-black"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}
