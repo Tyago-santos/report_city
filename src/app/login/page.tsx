aac4fcdeabd1c46bf57a6e965c8afb6d8d985241
Tiago Santos <xlzthyago@gmail.com>
2026-04-01 21:25:29 -0300

    Implementar link de rota entre as pagina de autenticação

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,15 @@
 import LoginForm from "@/src/components/LoginComponent";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-  <div className="flex h-screen items-center justify-center">
-    <LoginForm />
-  </div>
-  )
+    <div className="flex flex-col gap-4 h-screen items-center justify-center">
+      <h2 className="text-2xl font-bold">Faça seu login</h2>
+      <LoginForm />
+
+      <Link href="/register" className="text-blue-500 hover:underline">
+        Não tem uma conta? Cadastre-se
+      </Link>
+    </div>
+  );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,15 @@
 import RegisterForm from "@/src/components/RegisterComponent";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex flex-col gap-4 h-screen items-center justify-center">
+      <h2 className="text-2xl font-bold">Faça seu cadastro</h2>
       <RegisterForm />
+
+      <Link href="/login" className="text-blue-500 hover:underline">
+        Já tem uma conta? Faça login
+      </Link>
     </div>
   );
 }
