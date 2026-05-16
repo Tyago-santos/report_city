f540f4d65f1625f1fee3258a1475a1b75899cf0c
Tiago Santos <xlzthyago@gmail.com>
2026-05-16 13:24:25 -0300

    Implementar Props para rodar sem verificar hidratação do next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
