506c37eb9cb17fe24be98b9ac82fb8a635f0d4b6
Tiago Santos <xlzthyago@gmail.com>
2026-05-19 19:32:24 -0300

    Estilizar menu dektop refatorar código para menu

--- src/components/AsideComponent.tsx
+++ src/components/AsideComponent.tsx
@@ -1,24 +1,58 @@
 "use client";
 
 import Link from "next/link";
-import { LayoutList } from "lucide-react";
+import { LayoutList, HeartIcon, User2Icon, ImageIcon } from "lucide-react";
 
 export default function AsideComponent() {
+  const menu = [
+    {
+      name: "Feed",
+      href: "/",
+      icon: LayoutList,
+      active: true,
+    },
+
+    {
+      name: "Favoritos",
+      href: "/favorites",
+      icon: HeartIcon,
+      active: false,
+    },
+
+    {
+      name: "Minhas Postagens",
+      href: "/posts",
+      icon: ImageIcon,
+      active: false,
+    },
+    {
+      name: "Perfil",
+      href: "/perfil",
+      icon: User2Icon,
+      active: false,
+    },
+  ];
+
   return (
-    <aside aria-label="Menu lateral">
-      <Link className="block" href="/">
-        <LayoutList size={24} />
-        Feed
-      </Link>
-      <Link className="block" href="/favorites">
-        Favoritos
-      </Link>
-      <Link className="block" href="/posts">
-        Minhas Postagens
-      </Link>
-      <Link className="block" href="/perfil">
-        Perfil
-      </Link>
-    </aside>
+    <>
+      <aside
+        className={`fixed top-25 left-30 md:block hidden`}
+        aria-label="Menu lateral"
+      >
+        {menu.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={`flex gap-2 mb-5 font-semibold shadow-md hover:bg-primary hover:text-white p-4 rounded-sm ${item.active ? "bg-primary text-white" : "text-primary"}`}
+          >
+            <item.icon
+              className={`text-primary  hover:text-secondary ${item.active ? "text-white" : ""} `}
+              size={24}
+            />
+            {item.name}
+          </Link>
+        ))}
+      </aside>
+    </>
   );
 }
